e51586afa92af6350241a3c9f9879335c9d0c72b
sagun basnet <basnetsagun8@gmail.com>
2026-07-26 14:03:19 +0545

    js

---
+++ javascript/main.js
@@ -0,0 +1,31 @@
+// var a = 1;
+
+// a = "azsdfsdf"
+
+// const b = 12;
+
+// console.log(b);
+
+// console.log(a);
+
+// let a = 1;
+
+// a = "gfdfgj";
+
+// for (let i = 0; i == 10; i++) {
+//   console.log(i);
+// }
+
+// let i = 12;
+
+// let a = null;
+// let b = undefined;
+
+let a = 2;
+let b = 3;
+
+if (!(a > 1)) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
---
+++ javascript/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let a = 2;
+let b = 3;
+
+if (!(a > 1)) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}

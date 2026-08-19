aeb0362a9360b2640cdb7f8253aed9801ba7ce99
sagun basnet <basnetsagun8@gmail.com>
2026-08-19 14:04:23 +0545

    es6 day 1

---
+++ javascript/es6.js
@@ -0,0 +1,57 @@
+// const str = `asdfasdfasdfasdf ${name} . ${afdasdf} asdfasdfsdf`
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// const [a, b, c, d, e, f] = arr;
+
+// console.log(a, b, c, d, e, f);
+
+// const obj = {
+//   name: "Jhon",
+//   age: 16,
+//   email: "jhon@gmail.com",
+//   isVerified: true,
+//   arr: [1, 2, 3, 4, 5, 6, 7, 8],
+//   address: {
+//     country: "Nepal",
+//     state: "Koshi",
+//     city: "Itahari",
+//   },
+// };
+
+// const { address, name, email, arr } = obj;
+
+// console.log(name);
+
+// const myFun = ({ name, email }) => {
+//   console.log(name, email);
+// };
+// myFun(obj);
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// console.log(...arr);
+
+// const [a, b, c, d, ...rest] = arr;
+
+// console.log(rest);
+
+// const obj = {
+//   id: 12,
+//   name: "Jhon",
+//   age: 16,
+//   email: "jhon@gmail.com",
+//   isVerified: true,
+//   password: "1234567890",
+// };
+
+// const {password, ...others} = obj;
+
+// console.log(others);
+
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = [6, 7, 8, 9, 0];
+
+// const arr3 = [...arr1, ...arr2, 11, 12];
+
+// console.log(arr3);
--- javascript/main.js
+++ javascript/main.js
@@ -138,3 +138,4 @@
 // arr.map((item, index) => {
 //   console.log(item + " : " + index);
 // });
+

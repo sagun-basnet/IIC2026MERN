54411f9505212e17c273a33a9f8e88b1fd712c23
sagun basnet <basnetsagun8@gmail.com>
2026-08-21 14:04:52 +0545

    react

---
+++ react/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ react/src/Navbar.jsx
@@ -0,0 +1,6 @@
+const Navbar = () =>{
+    return(
+        <nav>This is navbar</nav>
+    )
+}
+export default Navbar

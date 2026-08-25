03fafa691444466aed13daef480bb51c80cb1769
sagun basnet <basnetsagun8@gmail.com>
2026-08-25 14:11:56 +0545

    tailwind

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,16 +1,18 @@
-import Navbar from "./Navbar";
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <h1 className="text-[4rem] font-bold p-[5.2rem] border-4 border-black bg-red- text-primary flex justify-center items-center">
+//         Hello
+//       </h1> */}
+//       <button className="p-2 px-4 rounded-md bg-blue-500 text-white font-bold cursor-pointer hover:bg-blue-700 transition-color ">
+//         Click
+//       </button>
+//     </div>
+//   );
+// };
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <h1>Hello</h1>
-    </div>
-  );
-};
+// export default App;
 
-export default App;
-
-// npm create vite@lates ==> to create framwork;
-// npm i ==> to install all packages;
-// npm run dev ==> to run project;
+// // npm create vite@lates ==> to create framwork;
+// // npm i ==> to install all packages;
+// // npm run dev ==> to run project;

83ae025422683fb9433e0f2372c10ed74dc95230
sagun basnet <basnetsagun8@gmail.com>
2026-09-02 13:51:22 +0545

    hooks

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
-import Navbar from "./Navbar";
-import Button from "./Button";
+// import React from "react";
+// import Navbar from "./Navbar";
+// import Button from "./Button";
+
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const name = "jhon";
+  // let count = 0;
+
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(true);
+
+  const addCount = () => {
+    // setData(true);
+    setCount((prev) => prev + 1);
+    console.log(count);
+  };
+
+  useEffect(() => {
+    console.log("From useEffect");
+  }, []);
 
   return (
     <>
-      <Navbar user={name} />
+      <div className="flex flex-col items-center justify-center">
+        {data ? <span>{count}</span> : <h1>Nothing to show</h1>}
+        <button
+          // onClick={()=> addCount(23)}
+          onClick={addCount}
+          className="p-2 px-4 bg-blue-500 text-white rounded-md"
+        >
+          +
+        </button>
+      </div>
+      {/* <Navbar user={name} /> */}
 
-      {name.length !== 0 && <h1 className="text-4xl">Welcome {name}</h1>}
+      {/* {name.length !== 0 && <h1 className="text-4xl">Welcome {name}</h1>} */}
       {/* <Button text="View More" color="blue" />
       <Button text="Login" color="red" />
       <Button text="Register" color="black" /> */}
